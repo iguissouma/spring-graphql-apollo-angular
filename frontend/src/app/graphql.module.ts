8be44f398812e150110478b885ae14cf694e98b8
Carlo Dapor <catull@gmail.com>
2018-12-11 15:00:42 +0100

    Various improvements
    - Typed arguments and methods
    - Generate entire GraphQL service from running Spring Boot instance
    - Corrected tests, now `ng test` actually runs w/o errors

--- frontend/src/app/graphql.module.ts
+++ frontend/src/app/graphql.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 import { Apollo, ApolloModule } from 'apollo-angular';
 import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpClientModule } from '@angular/common/http';
 import { ApolloLink } from 'apollo-link';
 
 @NgModule({
-  exports: [
-    HttpClientModule,
-    ApolloModule,
-    HttpLinkModule,
-  ]
+  exports: [HttpClientModule, ApolloModule, HttpLinkModule]
 })
 export class GraphQLModule {
   constructor(apollo: Apollo, httpLink: HttpLink) {
     const cache = new InMemoryCache();
-    const myAppLink: ApolloLink = httpLink.create({uri: '/graphql'});
+    const myAppLink: ApolloLink = httpLink.create({ uri: '/graphql' });
     apollo.create({
       link: myAppLink,
       cache: cache
